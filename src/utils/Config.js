c28710222084a2e5e989ce1c704711ac09645d6b
Mohammed Khawatme <mohammed.khawatme@modulacht.de>
2025-09-14 21:55:27 +0200

    Update log color styles, enhance attribute editor content, and improve cache management

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -10,15 +10,6 @@
  * @namespace
  */
 export const Config = {
-    /**
-     * The current version of the Power-Toolkit.
-     * @type {string}
-     */
-    TOOL_VERSION: '20.5',
-
-    /**
-     * The name of the developer.
-     * @type {string}
-     */
+    TOOL_VERSION: '20.5.1',
     DEVELOPER_NAME: 'Mohammed Khawatme'
 };
